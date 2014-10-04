5958c80dcf09a7db6a232be5d97f1d6384828297
Josh Kramer <josh@joshkra.me>
2014-10-04 16:06:00 -0400

    Initial commit. Should assume Angular instead of building with no library

---
+++ script/ktest.js
@@ -0,0 +1,23 @@
+(function(global, undefined) {
+	
+	var Ktest = function Ktest(_object_) {
+		var object = _object_;
+
+		// No internet - replace code with jquery or modal code
+
+		if(global.createElement) {
+			global.createElement('div');
+			
+		}
+
+
+		// No internet - replace code with jquery or modal code
+	};
+
+	if(global) {
+		global.Ktest = Ktest;
+	} else {
+		return Ktest;
+	}
+
+})(window)
---
+++ script/modal.js
@@ -0,0 +1,14 @@
+(function(global, undefined) {
+
+	var modal = function(object) {
+		
+	};
+
+
+	if(global) {
+		global.modal = modal;
+	} else {
+		return modal;
+	}
+
+})(window)
